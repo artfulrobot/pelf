6fa362f55e34a5f5c0df529732f8786bcbe107a1
Rich Lott / Artful Robot <forums@artfulrobot.uk>
2020-03-02 17:44:43 +0000

    catch lots of bits

---
+++ fix.php
@@ -0,0 +1,12 @@
+<?php
+// This does maths on all the rem font sizes. Because aah started with a 16px
+// rem size, but then bootstrap 3 insists on a 10px rem size, we had to adjust.
+// Script kept in case it is useful in future.
+//exit; // remove this in development if you want to use it.
+$file = "ang/pelf.css";
+$c = function ($m) {
+  return round($m[1]*1.6,5) . 'rem';
+};
+$src = file_get_contents($file);
+$dest = preg_replace_callback('/(\d+(?:\.\d+)?)(\s?rem)/', $c, $src);
+file_put_contents($file, $dest);
